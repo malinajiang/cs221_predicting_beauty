f19fc3602b33c16855b4cfc555da7640599aef8b
malinajiang <malinaj@stanford.edu>
2015-11-12 15:55:53 -0800

    added sh automation file

---
+++ automation.sh
@@ -0,0 +1,7 @@
+# File: automation.sh
+# --------------------
+# Automates the process of creating the dict of faces and running
+# our algorithm on the data.
+
+# python create_faces.py
+python get_face_data.py
